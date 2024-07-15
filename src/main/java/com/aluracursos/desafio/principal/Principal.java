df09de16583d958049a461663811775346ba5178
Brenda Ruiz <ruizhernandezbrendac@gmail.com>
2024-07-14 20:18:55 -0600

    2024-07-14 Top 10 libros más descargados

--- src/main/java/com/aluracursos/desafio/principal/Principal.java
+++ src/main/java/com/aluracursos/desafio/principal/Principal.java
@@ -1,8 +1,11 @@
 package com.aluracursos.desafio.principal;
 
 import com.aluracursos.desafio.model.Datos;
+import com.aluracursos.desafio.model.DatosLibros;
 import com.aluracursos.desafio.service.ConsumoAPI;
 import com.aluracursos.desafio.service.ConvierteDatos;
+
+import java.util.Comparator;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
@@ -14,5 +17,13 @@ public class Principal {
 
         var datos = conversor.obtenerDatos(json, Datos.class);
         System.out.println(datos);
+
+        // TOP 10 libros más descargados
+        System.out.println("TOP 10 libros más descargados");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
     }
 }
